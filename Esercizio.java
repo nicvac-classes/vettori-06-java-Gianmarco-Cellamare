9388317b8a75c4a1ba95792c6c627b49719dd206
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2024-11-28 15:18:36 +0000

    Vettori [06] java

--- Esercizio.java
+++ Esercizio.java
@@ -6,20 +6,46 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
+    public static void main(String args[]){
         Scanner in = new Scanner( System.in );
+        int n,i,iMin;
+        System.out.println ("Quanti atleti ci sono in gara?");
+        n=in.nextInt();
+        String [] nomi=new String[n];
+        String [] nazionali=new String[n];
+        Double [] tempi=new Double[n];
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        for(i=0; i <= n-1; i++){
+            System.out.println("nome " +i+1+ "° atleta: ");
+            nomi=in.nextline();
+            System.out.println("Nazionale " +i+1+ "° atleta: ");
+            nazionali=in.nextline();
+            System.out.println("Tempo " +i+1+ "° atleta: ");
+            tempi=in.nextDouble();
+        }
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        System.out.println("Informazioni inserite");
+        for (i = 0; i <= n -1; i++){
+            System.out.println(+i+1+ "° atleta: " +nomi[i]+ "; nazionale: " +nazionali[i]+ "; tempo" +tempi[i]);
+        }
+
+        System.ot.println("Ricerca dell'atleta vincitore");
+        double min;
+
+        iMin = 0;
+        min = tempi[0];
+        for (i = 0; i <nomi.length(); i++){
+            if (tempi[i] <min){
+                iMin = i;
+                min = tempi[i];
+            }
+        }
+
+        System.out.println("Atleta vincitore: " +nomi[iMin]+ "; nazionale: " +nazionali[iMin]+ "; tempo: " +tempi[iMin]);
+
+
+
+
     }
 }
 
